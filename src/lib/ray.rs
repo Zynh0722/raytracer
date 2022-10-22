b78086667e92d82c4440cb9c9f351e0dadbfd69a
Zynh0722 <Zynh0722@gmail.com>
2022-10-22 01:51:11 -0700

    make at a reference remove clone and copy from ray

--- src/lib/ray.rs
+++ src/lib/ray.rs
@@ -1,7 +1,7 @@
 use crate::Vec3;
 use crate::Point3;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug)]
 pub struct Ray {
     pub origin: Point3,
     pub direction: Vec3,
@@ -16,7 +16,7 @@ impl Ray {
         Self { origin: Point3::null(), direction }
     }
 
-    pub fn at(self, t: f64) -> Point3 {
+    pub fn at(&self, t: f64) -> Point3 {
         self.origin + (t * self.direction)
     }
 }
