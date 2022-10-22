9f997758ba7acfaa3022fc9d241b80b928760146
Zynh0722 <Zynh0722@gmail.com>
2022-10-22 04:50:01 -0700

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,10 @@ fn hit_sphere(center: &Point3, rad: f64, r: &Ray) -> f64 {
 
 fn ray_color(r: &Ray) -> Color {
     let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
-    
+
     if t > 0.0 {
         let n = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).normalized();
-        return 0.5 * Color::new(n.x+1.0, n.y+1.0, n.z+1.0);
+        return 0.5 * Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
     }
 
     let unit_direction = r.direction.normalized();
